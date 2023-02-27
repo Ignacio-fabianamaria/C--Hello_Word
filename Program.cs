f74b33e6271fd1d6e818ec32e5b9a90624d0fc55
Siigmaf <faby.tbe@gmail.com>
2023-02-27 13:26:20 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWprd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello, World!"); 
+           Console.WriteLine("Meu primeiro projeto com C#");
+        }
+    }
+}
+
