ffe9368758326979bbe12e19fe6a73df7d6da36a
Siigmaf <faby.tbe@gmail.com>
2023-02-28 12:23:09 -0300

    valores literais

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace HelloWorld
         {
            Console.WriteLine("Hello, World!"); 
            Console.WriteLine("Meu primeiro projeto com C#");
+
+           Console.WriteLine("---Valores literais---");
+
+           //char
+           Console.WriteLine("char:" + " " + "abcdef");
+           //int
+           Console.WriteLine("int:" + " " + 123456);
+           //decimal
+           Console.WriteLine("decimal:" + " " + 4.5m);
+           //bool
+           Console.WriteLine("bool:" + " " + true);
+           Console.WriteLine("bool:" + " " + false);
         }
     }
 }
