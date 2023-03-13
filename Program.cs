d4ad702a21bde3e5c3401e76f13ddd748bc44076
Siigmaf <faby.tbe@gmail.com>
2023-03-13 15:34:02 -0300

    formatação de saida do console

--- Program.cs
+++ Program.cs
@@ -5,48 +5,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Meu primeiro projeto com C#");
-            Console.WriteLine("---Declarando variáveis + valores literais---");
 
-            //char
-            char letra = 'a';
-            Console.WriteLine("char:" + " " + letra);
-            //int
-            int numero = 123456;
-            Console.WriteLine("int:" + " " + numero);
-            //decimal
-            decimal numeroDecimal = 4.5m;
-            Console.WriteLine("decimal:" + " " + numeroDecimal);
-            //bool
-            bool verdadeiro = true;
-            Console.WriteLine("bool:" + " " + verdadeiro);
-            bool falso = false;
-            Console.WriteLine("bool:" + " " + falso);
-            //string
-            string palavra = "amarelo";
-            Console.WriteLine("string:" + " " + palavra);
-            //double
-            double numeroDouble1 = 3.5;
-            double numeroDouble2 = 1.5;
-            double multplicadoubles = numeroDouble1 * numeroDouble2;
-            Console.WriteLine("double" + " " + multplicadoubles);
+            Console.WriteLine("\n-----FORMATANDO SAÍDA NO CONSOLE-----\n");
 
-            //var aceita qualquer dados
-            var idade2 = 35;
-            var nome = "Fabiana";
-            Console.WriteLine("Nome:" + " " + nome + " - " + "idade:" + " " + idade2);
+            // tipando as variáveis ao mesmo tempo
+            int n1, n2, n3, n4, n5;
+            double d1,d2,d3;
 
-            //Formatação
-            double x = 30.255;
-            Console.WriteLine(x.ToString("F1"));//formatando para float
-            Console.WriteLine(x.ToString("C"));//formatando para moeda corrente
-            Console.WriteLine(x.ToString("P"));//formatando para porcentagem
+            //declarando e atribuindo valor para variável;
+            n1 = 10; n2 = 15; n3 = 20; n4 = 25; n5 = 3;
+            d1 = 5.50; d3 = 0.1; d2= d1+(d1*d3);
+            string produto = "bolo";
 
-            // operações matemática
-            int num1 = 5, num2 = 3, res;//tipando 3variáveis ao mesmo tempo
-            res = num1 * num2;
-            Console.WriteLine("A multiplicação de " + num1 + " com " + num2 + " é: " + res);
+            //para imprimir de forma mais simplificada é usado indice para referencia a posição das variaveis
+            Console.WriteLine("Mesma linha:\n n1={0}, n2={1}, n3={2}, n4={3}, n5={4},", n1, n2, n3, n4, n5);
+
+            //para imprimir com quebra de linha, troca-se a virgula por \n
+            Console.WriteLine("Quebrando linha:\n n1={0}\n n2={1}\n n3={2}\n n4={3}\n n5={4},", n1, n2, n3, n4, n5);
+
+            //inserindo tabulação \t
+            Console.WriteLine("Tabulação:\n n1=\t{0}\n n2=\t{1}\n n3=\t{2}\n n4=\t{3}\n n5=\t{4}\n", n1, n2, n3, n4, n5);
+
+            Console.WriteLine("Produto...............:{0,11}",produto);//indice '0' com tabulação de '11'
+             Console.WriteLine("Valor de compra......:{0,15:c}",d1);//indice '0' com tabulação de '15' e formato monetário :c
+              Console.WriteLine("Lucro...............:{0,16:p}",d3);//indice '0' com tabulação de '16' e formato de porcentagem :p
+               Console.WriteLine("Valor de venda.....:{0,17:c}",d2);//indice '0' com tabulação de '17' e formato monetário :c
+
+
+
 
 
         }
