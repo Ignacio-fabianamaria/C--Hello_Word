4976d8c1f06c4597732258824238b6efe96e61ed
Siigmaf <faby.tbe@gmail.com>
2023-04-07 11:26:24 -0300

    variavveis e valores literais

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Meu primeiro projeto com C#");
+            
             Console.WriteLine("---Declarando variáveis + valores literais---");
 
             //char
